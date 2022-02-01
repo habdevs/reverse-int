5fc2e34b6e25c44c6e40d3285cb0bcc878a581e8
Anton <hramickih@yandex.ru>
2022-02-01 13:11:01 +0300

    fix: add a new solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 module.exports = function reverse(n) {
-    return parseFloat(
-        n.toString().split('').reverse().join('')
-    )
+
+    const str = String(n);
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]
+    }
+
+    return parseFloat(newStr)
+
+
+    // const str = String(n);
+    // let newStr = '';
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     newStr += str[i]
+    // }
+
+    // console.log(reverse(100));
+
+
+    // const result = str.split('').reverse('').join('');
+    // return parseFloat(result);
+
+
+
+
+    // return parseFloat(
+    //     n.toString().split('').reverse().join('')
+    // )
 }
